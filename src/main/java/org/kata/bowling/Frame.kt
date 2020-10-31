5801c755adb4e768be836f7613c19c0e10eb4dd5
ktsal <tsalikispk@gmail.com>
2020-10-31 14:51:27 +0200

    Move Frame to standalone class

---
+++ src/main/java/org/kata/bowling/Frame.kt
@@ -0,0 +1,37 @@
+package org.kata.bowling
+
+class Frame {
+
+    private var lastRoll = 0
+    private var spare = false
+    private var strike = false
+    private var strikeBonusAdded = 0
+
+    fun roll(pins: Int) {
+        if (pins == 10) {
+            strike = true
+        } else if (lastRoll + pins == 10) {
+            spare = true
+        }
+        this.lastRoll = pins
+    }
+
+    fun bonus(pins: Int): Int {
+        if (strike && strikeBonusAdded <= 2) {
+            ++strikeBonusAdded
+            resetStrike()
+            return pins
+        } else if (spare) {
+            spare = false
+            return pins
+        }
+        return 0
+    }
+
+    private fun resetStrike() {
+        if (strikeBonusAdded == 2) {
+            strike = false
+            strikeBonusAdded = 0
+        }
+    }
+}
--- src/main/java/org/kata/bowling/Game.kt
+++ src/main/java/org/kata/bowling/Game.kt
@@ -16,39 +16,4 @@ class Game {
         return pins + bonus
     }
 
-    inner class Frame {
-
-        private var lastRoll = 0
-        private var spare = false
-        private var strike = false
-        private var strikeBonusAdded = 0
-
-        fun roll(pins: Int) {
-            if (pins == 10) {
-                strike = true
-            } else if (lastRoll + pins == 10) {
-                spare = true
-            }
-            this.lastRoll = pins
-        }
-
-        fun bonus(pins: Int): Int {
-            if (strike && strikeBonusAdded <= 2) {
-                ++strikeBonusAdded
-                resetStrike()
-                return pins
-            } else if (spare) {
-                spare = false
-                return pins
-            }
-            return 0
-        }
-
-        private fun resetStrike() {
-            if (strikeBonusAdded == 2) {
-                strike = false
-                strikeBonusAdded = 0
-            }
-        }
-    }
 }
