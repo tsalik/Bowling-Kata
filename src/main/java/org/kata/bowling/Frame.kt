01381e5b4d37fa0397a3af152bdf30fe27b03642
ktsal <tsalikispk@gmail.com>
2020-11-01 15:20:46 +0200

    remove unnecessary bonus calculation and fields from Frame

--- src/main/java/org/kata/bowling/Frame.kt
+++ src/main/java/org/kata/bowling/Frame.kt
@@ -5,42 +5,11 @@ data class Frame(
     private val secondThrow: Int? = null
 ) {
 
-    private var lastRoll = 0
-    private var spare = false
-    private var strike = false
-    private var strikeBonusAdded = 0
-
     fun roll(pins: Int): Frame {
-        if (pins == 10) {
-            strike = true
-        } else if (lastRoll + pins == 10) {
-            spare = true
-        }
-        this.lastRoll = pins
-
         return if (firstThrow == null) {
             Frame(firstThrow = pins)
         } else {
             Frame(firstThrow, secondThrow = pins)
-        }
-    }
-
-    fun bonus(pins: Int): Int {
-        if (strike && strikeBonusAdded <= 2) {
-            ++strikeBonusAdded
-            resetStrike()
-            return pins
-        } else if (spare) {
-            spare = false
-            return pins
-        }
-        return 0
-    }
-
-    private fun resetStrike() {
-        if (strikeBonusAdded == 2) {
-            strike = false
-            strikeBonusAdded = 0
         }
     }
 
